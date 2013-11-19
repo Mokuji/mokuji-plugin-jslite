b551d71025aa17a503ec50ba266dd06d2abb4fc4
Avaq <aldwin.vlasblom@gmail.com>
2013-11-19 15:45:31 +0000

    Update JSLite

--- plugin.php
+++ plugin.php
@@ -1,1 +1,1 @@
-<script type="text/javascript" charset="UTF-8" src="<?php echo $plugin ?>JSLite/min/classes.js"></script>
+<script type="text/javascript" charset="UTF-8" src="<?php echo $plugin ?>JSLite/min/classes+events.js"></script>
